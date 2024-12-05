cfb7a74fc92ceafc29745dcd94f8b6b32468a236
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2024-12-05 18:21:34 +0100

    Solve day 5 part 2

--- day5.js
+++ day5.js
@@ -99,11 +99,68 @@ part1Solve?.addEventListener("click", () => {
   stats?.replaceChildren(`${duration} ms`);
 });
 
+/**
+ * @param {[string, string][]} rules
+ * @param {string[]} numbers
+ *
+ * @returns {string[]} the subset of rules that apply to `numbers`
+ */
+function getRules(rules, numbers) {
+  return rules.filter(([l, r]) => {
+    let foundL = false;
+    let foundR = false;
+    for (let n of numbers) {
+      switch (n) {
+        case l:
+          foundL = true;
+          break;
+        case r:
+          foundR = true;
+          break;
+      }
+
+      if (foundL && foundR) return true;
+    }
+
+    return false;
+  });
+}
+
+/**
+ * @param {[string, string][]} rules
+ *
+ * @returns {string[]} a list of numbers ordered according to the rules, except the last number which we don't need anyway.
+ */
+function getNumbers(rules) {
+  const counts = new Map();
+  for (let [l] of rules) {
+    counts.set(l, (counts.get(l) ?? 0) + 1);
+  }
+  return [...counts.entries()].sort((a, b) => b[1] - a[1]).map(([n]) => n);
+}
+
 part2Solve.addEventListener("click", () => {
   const start = performance.now();
-  const input = inputElm.value.trim().split(/\s+/);
+  const input = inputElm.value.trim().split("\n");
+
   let result = 0;
 
+  let i = 0;
+  for (; i < input.length && input[i] !== ""; ++i) {}
+  const rules = input.slice(0, i).map((rule) => rule.split("|"));
+
+  ++i;
+
+  for (; i < input.length; ++i) {
+    const numbers = input[i].trim().split(",");
+    const ordered = getNumbers(getRules(rules, numbers));
+    // ordered is missing the last number due to how getNumbers is implemented,
+    // but we actually don't need that number since the ordered list cannot differ only at the last index
+    // we just need to not subtract 1 when finding the middle.
+    if (ordered.some((value, index) => value !== numbers[index])) {
+      result += parseInt(ordered[ordered.length / 2]);
+    }
+  }
   const duration = performance.now() - start;
 
   const answer = document.getElementById("part2-answer");
