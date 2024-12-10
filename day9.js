e5d30775a288a27d4dac32dc9faa2179bae5921d
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2024-12-10 17:54:59 +0100

    Add solution to day 9 part 2

--- day9.js
+++ day9.js
@@ -47,9 +47,54 @@ part1Solve.addEventListener("click", () => {
 
 part2Solve.addEventListener("click", () => {
   const start = performance.now();
-  const input = inputElm.value.trim().split("\n");
+  const input = inputElm.value.trim();
 
   let result = 0;
+
+  const blocks = [];
+  for (let i = 0; i < input.length; ++i) {
+    if (i % 2 === 0) {
+      for (let j = 0; j < input[i]; ++j) {
+        blocks.push(i / 2);
+      }
+    } else {
+      for (let j = 0; j < input[i]; ++j) {
+        blocks.push(null);
+      }
+    }
+  }
+
+  const files = [];
+  for (let i = 0; i < blocks.length; ) {
+    if (blocks[i] === null) {
+      for (; i < blocks.length && blocks[i] === null; ++i) {}
+    }
+    const id = blocks[i];
+    const addr = i;
+    for (; i < blocks.length && blocks[i] === id; ++i) {}
+    const size = i - addr;
+    files.push({ addr, size });
+  }
+
+  for (let i = files.length - 1; i >= 0; --i) {
+    const file = files[i];
+    for (let j = 0; j < file.addr; ++j) {
+      const next = blocks.slice(j, j + file.size);
+      if (next.length === file.size && next.every((block) => block === null)) {
+        for (let k = 0; k < file.size; ++k) {
+          blocks[j + k] = i;
+          blocks[file.addr + k] = null;
+        }
+        break;
+      }
+    }
+  }
+
+  for (let i = 0; i < blocks.length; ++i) {
+    if (blocks[i] !== null) {
+      result += i * blocks[i];
+    }
+  }
 
   const duration = performance.now() - start;
 
