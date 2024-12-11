b9467725af00b6f2ba4f18aeab4438159c558685
Rolf Christian Jørgensen <27211248+rcjorgensen@users.noreply.github.com>
2024-12-11 08:16:35 +0100

    Add solution to day 7 part 1

--- day7.js
+++ day7.js
@@ -1,46 +1,46 @@
-const inputElm = document.getElementById("input");
+import { init, inputElm, part1Solve, part2Solve } from "./common.js";
 
-const storedInput = localStorage.getItem("day8-input");
-if (storedInput !== null) {
-  inputElm.value = storedInput;
-}
+init("day7");
 
-const storedScroll = localStorage.getItem("day8-input-scroll");
-if (storedScroll !== null) {
-  inputElm.scrollTop = parseInt(storedScroll);
-}
+/**
+ * @param {number} target
+ * @param {string[]} operands
+ * @param {number} result
+ *
+ * @returns {boolean} true if the result is equal to the target.
+ */
+function search(target, operands, result) {
+  if (result > target) {
+    return false;
+  }
 
-const storedSize = localStorage.getItem("day8-input-size");
-if (storedSize !== null) {
-  const { width, height } = JSON.parse(storedSize);
-  inputElm.style.width = `${width}px`;
-  inputElm.style.height = `${height}px`;
-}
+  if (operands.length === 0) {
+    return result === target;
+  }
 
-window.onbeforeunload = () => {
-  localStorage.setItem("day8-input-scroll", inputElm.scrollTop);
-  localStorage.setItem(
-    "day8-input-size",
-    JSON.stringify({
-      width: inputElm.offsetWidth,
-      height: inputElm.offsetHeight,
-    }),
+  const operand = parseInt(operands[0]);
+  const rest = operands.slice(1);
+
+  return (
+    search(target, rest, result + operand) ||
+    search(target, rest, result * operand)
   );
-};
+}
 
-const inputSaveBtn = document.getElementById("input-save");
-inputSaveBtn.addEventListener("click", () => {
-  localStorage.setItem("day8-input", inputElm.value);
-});
-
-const part1Solve = document.getElementById("part1-solve");
-const part2Solve = document.getElementById("part2-solve");
-
-part1Solve?.addEventListener("click", () => {
+part1Solve.addEventListener("click", () => {
   const start = performance.now();
   const input = inputElm.value.trim().split("\n");
 
   let result = 0;
+
+  for (const line of input) {
+    const [lhs, rhs] = line.trim().split(":");
+    const operands = rhs.trim().split(" ");
+    const target = parseInt(lhs);
+    if (search(target, operands.slice(1), parseInt(operands[0]))) {
+      result += target;
+    }
+  }
 
   const duration = performance.now() - start;
 
